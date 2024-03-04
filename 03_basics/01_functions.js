31c82372775c762333eb37f73c1f8d6265e2db0e
AnkitaDes <desai_ankita@outlook.com>
2024-03-04 17:45:17 -0500

    functions object array parameters

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -45,4 +45,38 @@ function loginUserMessage(username = "sam") {
 }
 // console.log(loginUserMessage("Sammy"));
 //console.log(loginUserMessage(""));
-console.log(loginUserMessage());
+//console.log(loginUserMessage());
+
+//...num1 rest operator puts all the values in an array
+// function calculateCartPrice(...num1) {
+//   return num1;
+// }
+// console.log(calculateCartPrice(200, 400, 500, 2000)); //[ 200, 400, 500, 2000]
+
+// function calculateCartPrice(val1, val2, ...num1) {
+//   return num1;
+// }
+// console.log(calculateCartPrice(200, 400, 500, 2000)); //[ 500, 2000]
+
+const user = {
+  username: "Sammy",
+  price: 199,
+};
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+handleObject(user);
+handleObject({
+  username: "Sam",
+  price: 399,
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));

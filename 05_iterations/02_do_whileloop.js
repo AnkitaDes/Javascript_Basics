488775f91bfd01f2be0d4763286f0e51c913ced7
AnkitaDes <desai_ankita@outlook.com>
2024-03-05 13:19:33 -0500

    whileloop

---
+++ 05_iterations/02_do_whileloop.js
@@ -0,0 +1,25 @@
+//WHILE LOOP
+
+// let index = 0;
+// while (index <= 10) {
+//   console.log(`Value of Index is ${index}`);
+//   index = index + 2;
+// }
+
+// let myArray = ["Flash", "Superman", "Spiderman"];
+// console.log("myArray length: ", myArray.length);
+
+// let i = 0;
+// while (i < myArray.length) {
+//   console.log(`Value is ${myArray[i]}`);
+//   i++;
+// }
+
+// DO WHILE LOOP
+
+let score = 100;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);

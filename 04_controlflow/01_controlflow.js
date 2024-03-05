156b4cbce6ff488b2dbd7fc8ccbd3731e6133873
AnkitaDes <desai_ankita@outlook.com>
2024-03-05 12:13:57 -0500

    operators

--- 04_controlflow/01_controlflow.js
+++ 04_controlflow/01_controlflow.js
@@ -61,7 +61,26 @@ if (loggedInFromEmail || loggedInFromGmail) {
   console.log("User logged in ");
 }
 
+//Logical Operators
 // &&  and  both true
 // ||  or  one should be true
 
 // connditional rendering in React
+
+//Nullish Coalescing Operator: null and undefined
+// used to handle errors when getting data from backend
+
+// if its some value the variable gets the first value if null or undefined the first value after null or undefined is assigned to the variable
+let num1;
+// num1 = 5 ?? 10;
+// num1 = null ?? 10;
+// num1 = undefined ?? 15;
+num1 = null ?? 10 ?? 20;
+console.log(num1);
+
+// Ternary Operator shorthand if else
+
+//condition ? true : false;
+const iceTeaPrice = 100;
+
+iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("More than 80");
